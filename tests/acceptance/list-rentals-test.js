f64d4c2a8ffc922819eec1c0c7ba994d316dfc4d
viveksriram1 <vivek.sriram@kiprosh.com>
2017-04-14 13:33:02 +0530

    add demo data for rentals and test listing rentals

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -4,13 +4,34 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 moduleForAcceptance('Acceptance | list rentals');
 
 test('should show rentals as the home page', function(assert){
+  visit('/');
+  andThen(function(){
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+  })
 });
 
-test('should link to information about the company.',function(assert){});
+test('should link to information about the company.',function(assert){
+  visit('/');
+  click('a:contains("About")');
+  andThen(function(){
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  })
+});
 
-test('should link to contactinformation ',function(assert){});
+test('should link to contactinformation ',function(assert){
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function(){
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  })
+});
 
-test('should list available rentals.',function(assert){});
+test('should list available rentals.',function(assert){
+  visit('/');
+  andThen(function(){
+    assert.equal(find('.listing').length, 3, 'should see 3 listing of rentals.')
+  })
+});
 
 test('should filter the list of rentals by city',function(assert){});
 
